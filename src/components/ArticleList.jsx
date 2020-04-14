1cf656c6169d61a96e79c96d624db873957e7a98
James Cowell <james.cowell28@gmail.com>
2020-04-14 11:56:43 +0100

    added Header, TopicList and ArticleList components

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ArticleList extends React.Component {
+  render() {
+    return (
+      <ul className="ArticleList">
+        <li>Articles</li>
+        <li>Go</li>
+        <li>Here</li>
+      </ul>
+    );
+  }
+}
+
+export default ArticleList;
---
+++ src/components/TopicList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TopicList extends React.Component {
+  render() {
+    return (
+      <ul className="TopicList">
+        <li>Topic</li>
+        <li>List</li>
+        <li>Here</li>
+      </ul>
+    );
+  }
+}
+
+export default TopicList;
